9b2c029d9511b7073576788d715338748d6a56f3
Sidney Wimart <sidney@sidneys-macbook-pro.home>
2019-08-05 13:30:05 +0200

    Dashboard Fix

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import ExpenseList from './ExpenseList';
 import ExpensesTotal from './ExpensesTotal';
 import ExpenseListFilters from './ExpenseListFilters';
-import ExpenseListItems from './ExpenseListItems';
+
 
 const ExpenseDashboardPage = () => (
     <div>
